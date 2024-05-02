311b5b08ae7d1708d18924d462b8ac5021b126f0
leslie alvarado <alvarado.leslie12@yahoo.com>
2024-05-02 10:45:18 -0700

    Created and added css

---
+++ demo.js
@@ -0,0 +1,31 @@
+function generateName() {
+  const names = [
+    "Niran Abdulqader",
+    "Alfi Ali",
+    "Leslie Alvarado",
+    "Dante Angerame",
+    "Guadalupe Argumedo Saucedo",
+    "Rene Banuelos",
+    "Kadi Barry",
+    "Jaranesca Braker",
+    "Victor Castaneda",
+    "Andrea Ceballos",
+    "Uriel Delgado",
+    "Kaaliyah Gray",
+    "Kayla Hanifen",
+    "Moncerrat Lua Salas",
+    "Max Mayo",
+    "Jesus MImila",
+    "Sabrina Robles",
+    "Abdikadir Samatar",
+    "Daniel Solar",
+    "Brett Spokes"
+  ];
+
+  // Randomly select a name
+  const index = Math.floor(Math.random() * names.length);
+
+  document.getElementById("displayGreet").innerHTML = "Hello there, <span class='highlight'>" + names[index] + "</span> !";
+}
+// Example usage:
+generateName();
